8ff0736975525923e34651f3188e1abf8295e83b
이지현 <leejihyun0324@gmail.com>
2024-04-30 16:05:11 +0900

    📦️ Chore: 디렉토리 구조 수정 및 eslint 규칙 추가 (#5)
    * 🚚 Rename: hooks, styles, utils 디렉토리 위치 변경
    
    * 📦️ Chore: eslint 규칙 추가 및 적용
    
    * 📦️ Chore: constants 폴더 추가
    
    * 📦️ Chore: eslint 규칙 추가
    
    * 📦️ Chore: assets 폴더 추가

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
-const LadingPage = () => (
-  <div>
-    <h1>랜딩 페이지입니다.</h1>
-  </div>
-);
+const LadingPage = () => {
+  return (
+    <div>
+      <h1>랜딩 페이지입니다.</h1>
+    </div>
+  );
+};
 
 export default LadingPage;
--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import Sample from '@/components/Sample/Sample';
 
 const TestPage = () => {
-  const test = 1;
-
   return (
     <div>
-      <p>{test}</p>
+      <p>공통 컴포넌트 만든거는 test 페이지에서 테스트합니다.</p>
+      <Sample />
     </div>
   );
 };
