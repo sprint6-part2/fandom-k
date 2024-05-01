1b1710db1b795ea37823ff6558874c6853dcd2a9
Bersk3r <kiscwook@gmail.com>
2024-05-01 23:07:28 +0900

    🐛 Fix: 잘못된 테스트 파일 수정

--- src/components/Sample/Sample.jsx
+++ src/components/Sample/Sample.jsx
@@ -1,16 +1,5 @@
-import useModal from '../../hooks/useModal';
-import Modal from '@/Modal';
-
 const Sample = () => {
-  const [isOpen, openModal, closeModal] = useModal();
-  return (
-    <div>
-      <button onClick={openModal}>Open Modal</button>
-      <Modal isOpen={isOpen} title="모달 타이틀" onClose={closeModal}>
-        <div style={{ color: '#fff' }}>바디입니다.</div>
-      </Modal>
-    </div>
-  );
+  return <div>안녕</div>;
 };
 
 export default Sample;
--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -1,10 +1,19 @@
 import Sample from '@/components/Sample/Sample';
+import useModal from '../../hooks/useModal';
+import Modal from '@/Modal';
 
 const TestPage = () => {
+  const [isOpen, openModal, closeModal] = useModal();
+
   return (
     <div>
       <p>공통 컴포넌트 만든거는 test 페이지에서 테스트합니다.</p>
       <Sample />
+
+      <button onClick={openModal}>Open Modal</button>
+      <Modal isOpen={isOpen} title="모달" onClose={closeModal}>
+        <div style={{ color: '#fff' }}>바디입니다.</div>
+      </Modal>
     </div>
   );
 };
