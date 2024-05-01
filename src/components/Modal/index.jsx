af73fd6da6913a84297b40f758a41ad8a2f7b9ff
Bersk3r <kiscwook@gmail.com>
2024-05-01 16:13:20 +0900

    ♻️ Refactor: 기존 Modal 및 테스트 컴포넌트 내용 작성

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import style from './Modal.module.scss';
+import cn from 'classnames';
 
-const Modal = ({ isOpen, title, children, closeModal }) => {
-  return <div>{children}</div>;
+const Modal = ({ isOpen, title, onClose, children, size }) => {
+  const isBig = size === 'big';
+  return (
+    <>
+      {isOpen ? (
+        <div className={cn(style.ModalBackDrop, { [style.big]: isBig })}>
+          <div className={cn(style.ModalView)}>
+            <div className={cn(style.ModalViewHeader)}>
+              {title !== null ? <h2>{title}</h2> : null}
+              <button onClick={onClose}>X</button>
+            </div>
+            {children}
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
 };
 
 export default Modal;
--- src/components/Sample/Sample.jsx
+++ src/components/Sample/Sample.jsx
@@ -1,5 +1,16 @@
+import useModal from '../../hooks/useModal';
+import Modal from '@/Modal';
+
 const Sample = () => {
-  return <div>안녕</div>;
+  const [isOpen, openModal, closeModal] = useModal();
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal isOpen={isOpen} title="모달 타이틀" onClose={closeModal}>
+        <div style={{ color: '#fff' }}>바디입니다.</div>
+      </Modal>
+    </div>
+  );
 };
 
 export default Sample;
