402674b50843758273788bd5422dc46ad1489608
Bersk3r <kiscwook@gmail.com>
2024-05-01 23:10:22 +0900

    ♻️ Refactor: 바깥 클릭 시 모달이 닫히도록 구현

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
-import style from './Modal.module.scss';
-import cn from 'classnames';
+import style from '@/components/Modal/Modal.module.scss';
+import ModalCloseButton from '@/assets/icons/ModalCloseButton';
+import classNames from 'classnames';
 
 const Modal = ({ isOpen, title, onClose, children, size }) => {
+  // isBig은 임시로 둔 변수입니다.
+  // 추후에 크기에 따른 값을 설정할 때, 사용할 예정입니다.
   const isBig = size === 'big';
+  const isTitle = title !== null;
+
   return (
     <>
-      {isOpen ? (
-        <div className={cn(style.ModalBackDrop, { [style.big]: isBig })}>
-          <div className={cn(style.ModalView)}>
-            <div className={cn(style.ModalViewHeader)}>
-              {title !== null ? <h2>{title}</h2> : null}
-              <button onClick={onClose}>X</button>
+      {isOpen && (
+        <div className={classNames(style.ModalBackDrop)} onClick={onClose}>
+          <div
+            className={classNames(style.ModalView, { [style.big]: isBig })}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div
+              className={classNames(style.ModalViewHeader, {
+                [style.titleActive]: isTitle,
+              })}
+            >
+              {title && <h2>{title}</h2>}
+              <ModalCloseButton
+                className={classNames(style.ModalCloseButton)}
+                onClick={onClose}
+              />
             </div>
             {children}
           </div>
         </div>
-      ) : null}
+      )}
     </>
   );
 };
