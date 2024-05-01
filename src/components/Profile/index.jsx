edafe88aca3f9b3042ff562c813144cfbe69050a
easyhyun00 <leejihyun0324@gmail.com>
2024-05-01 17:47:01 +0900

    ✨ Feat: 프로필 select 구현

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import classNames from 'classnames';
 import style from './styles.module.scss';
+import CheckIcon from '@/assets/icons/Check';
 
 const ProFile = ({
   imageUrl = 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg',
   size = 'lg',
+  selected = false,
 }) => {
-  const profileClass = classNames(style.profile, size);
-  const imageClass = classNames(style.image, size);
+  const profileClass = classNames(style.profile, style[size]);
+  const imageClass = classNames(style.image, style[size]);
+  const selectClass = classNames(style.selected, style[size]);
+  const iconClass = classNames(style.check, style[size]);
 
   return (
     <div className={profileClass}>
+      {selected && (
+        <>
+          <div className={selectClass} />
+          <CheckIcon className={iconClass} />
+        </>
+      )}
       <img className={imageClass} src={imageUrl} alt="프로필 사진" />
     </div>
   );
--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -2,10 +2,24 @@ import ProFile from '@/components/Profile';
 
 const TestPage = () => {
   return (
-    <div>
-      <ProFile />
-      <ProFile size="md" />
-      <ProFile size="sm" />
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <ProFile
+        selected
+        imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493431544/rllze1.jpeg"
+      />
+      <ProFile
+        size="bg"
+        imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493561949/theboyz1.jpeg"
+      />
+      <ProFile
+        size="md"
+        selected
+        imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493201646/nct1271.jpeg"
+      />
+      <ProFile
+        size="sm"
+        imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714492902115/seven1.jpeg"
+      />
     </div>
   );
 };
