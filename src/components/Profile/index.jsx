07b5ba615739c72065ccac0584e2289aa99348f8
easyhyun00 <leejihyun0324@gmail.com>
2024-05-01 18:32:06 +0900

    ✨ Feat: 프로필 상단 엑스 아이콘 구현

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,24 +1,39 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import classNames from 'classnames';
 import style from './styles.module.scss';
 import CheckIcon from '@/assets/icons/Check';
+import ProfileXIcon from '@/assets/icons/ProfileX';
 
 const ProFile = ({
   imageUrl = 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg',
   size = 'lg',
+  clicked = false,
   selected = false,
+  onClickProfile = () => {},
+  onClickDelete = () => {},
 }) => {
   const profileClass = classNames(style.profile, style[size]);
   const imageClass = classNames(style.image, style[size]);
-  const selectClass = classNames(style.selected, style[size]);
-  const iconClass = classNames(style.check, style[size]);
+  const clickedClass = classNames(style.clicked, style[size]);
+  const checkClass = classNames(style.check, style[size]);
+  const selectedClass = classNames(style.selected, style[size]);
+
+  const handleDeleteClick = (e) => {
+    e.stopPropagation();
+    onClickDelete();
+  };
 
   return (
-    <div className={profileClass}>
+    <div className={profileClass} onClick={onClickProfile}>
       {selected && (
+        <ProfileXIcon className={selectedClass} onClick={handleDeleteClick} />
+      )}
+      {clicked && (
         <>
-          <div className={selectClass} />
-          <CheckIcon className={iconClass} />
+          <div className={clickedClass} />
+          <CheckIcon className={checkClass} />
         </>
       )}
       <img className={imageClass} src={imageUrl} alt="프로필 사진" />
--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -4,20 +4,34 @@ const TestPage = () => {
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <ProFile
-        selected
+        clicked
+        onClickProfile={() => {
+          return console.log('프로필사진 누름');
+        }}
         imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493431544/rllze1.jpeg"
       />
       <ProFile
         size="bg"
+        selected
+        onClickDelete={() => {
+          return console.log('삭제아이콘 누름');
+        }}
         imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493561949/theboyz1.jpeg"
       />
       <ProFile
         size="md"
-        selected
+        clicked
+        onClickProfile={() => {
+          return console.log('프로필사진 누름');
+        }}
         imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493201646/nct1271.jpeg"
       />
       <ProFile
         size="sm"
+        selected
+        onClickDelete={() => {
+          return console.log('삭제아이콘 누름');
+        }}
         imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714492902115/seven1.jpeg"
       />
     </div>
