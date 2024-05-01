74e86d771e072f313998cfe0c1eda321633bcf38
easyhyun00 <leejihyun0324@gmail.com>
2024-05-01 18:43:17 +0900

    📚 Docs: Profile 컴포넌트 param 주석 작성

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -6,6 +6,15 @@ import style from './styles.module.scss';
 import CheckIcon from '@/assets/icons/Check';
 import ProfileXIcon from '@/assets/icons/ProfileX';
 
+/**
+ * @param {imageUrl} 이미지 주소
+ * @param {size} 프로필 사이즈 (lg, bg, md, sm)
+ * @param {clicked} 프로필 클릭 유무 (핑크색)
+ * @param {selected} 프로필 선택 유무 (상단 엑스)
+ * @param {onClickProfile} 프로필 클릭 함수
+ * @param {onClickDelete} 상단 엑스 클릭 함수
+ * @returns
+ */
 const ProFile = ({
   imageUrl = 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg',
   size = 'lg',
