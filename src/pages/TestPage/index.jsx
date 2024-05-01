fe916be34439c8cf2863a94112d5f76149c124a8
CHAE <cowhdals1111@gmail.com>
2024-05-01 18:49:52 +0900

    ✨ 헤더 구현
    반응형,기능 구현

--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -1,8 +1,10 @@
+import Header from '@/components/Header';
 import Sample from '@/components/Sample/Sample';
 
 const TestPage = () => {
   return (
     <div>
+      <Header />
       <p>공통 컴포넌트 만든거는 test 페이지에서 테스트합니다.</p>
       <Sample />
     </div>
