40583fe447243f71fa536bf76ee6d6c0e70d4651
Bersk3r <kiscwook@gmail.com>
2024-05-02 16:36:48 +0900

    ♻️ Refactor: 반응형 처리를 위한 모달 헤더 부분 변경

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,38 +1,48 @@
 import React from 'react';
-import style from '@/components/Modal/Modal.module.scss';
+import style from '@/components/Modal/styles.module.scss';
 import ModalCloseButton from '@/assets/icons/ModalCloseButton';
+import BackButton from '@/assets/icons/BackButton';
 import classNames from 'classnames';
 
-const Modal = ({ isOpen, title, onClose, children, size }) => {
+const Modal = ({ isOpen, title, onClose, children, isFull = false }) => {
   // isBig은 임시로 둔 변수입니다.
   // 추후에 크기에 따른 값을 설정할 때, 사용할 예정입니다.
-  const isBig = size === 'big';
   const isTitle = title !== null;
 
   return (
-    <>
+    <div>
       {isOpen && (
-        <div className={classNames(style.ModalBackDrop)} onClick={onClose}>
-          <div
-            className={classNames(style.ModalView, { [style.big]: isBig })}
-            onClick={(e) => e.stopPropagation()}
-          >
+        <div className={style.ModalBackDrop} onClick={onClose}>
+          <div className={style.ModalView} onClick={(e) => e.stopPropagation()}>
             <div
               className={classNames(style.ModalViewHeader, {
                 [style.titleActive]: isTitle,
+                [style.fullActive]: isFull,
               })}
             >
-              {title && <h2>{title}</h2>}
-              <ModalCloseButton
-                className={classNames(style.ModalCloseButton)}
-                onClick={onClose}
-              />
+              {title && (
+                <h2
+                  className={classNames(style.HeaderTitle, {
+                    [style.fullActive]: isFull,
+                  })}
+                >
+                  {title}
+                </h2>
+              )}
+              {isFull ? (
+                <BackButton
+                  className={style.HeaderBackButton}
+                  onClick={onClose}
+                />
+              ) : (
+                <ModalCloseButton onClick={onClose} />
+              )}
             </div>
             {children}
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
