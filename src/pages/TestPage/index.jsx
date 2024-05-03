308d271dc4de78b7bc4d8c95e852de2d5cb9c0a2
easyhyun00 <leejihyun0324@gmail.com>
2024-05-03 14:06:51 +0900

    🔨 Refactor: 테스트페이지 테스트컴포넌트 위치 수정

--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -9,47 +9,48 @@ const TestPage = () => {
   const [isOpen, openModal, closeModal] = useModal();
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <Profile
-        clicked
-        onClickProfile={() => {
-          return console.log('프로필사진 누름');
-        }}
-        imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493431544/rllze1.jpeg"
-      />
-      <Profile
-        size="bg"
-        selected
-        onClickDelete={() => {
-          return console.log('삭제아이콘 누름');
-        }}
-        imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493561949/theboyz1.jpeg"
-      />
-      <Profile
-        size="md"
-        clicked
-        onClickProfile={() => {
-          return console.log('프로필사진 누름');
-        }}
-        imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493201646/nct1271.jpeg"
-      />
-      <Profile
-        size="sm"
-        selected
-        onClickDelete={() => {
-          return console.log('삭제아이콘 누름');
-        }}
-        imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714492902115/seven1.jpeg"
-      />
-      <p>공통 컴포넌트 만든거는 test 페이지에서 테스트합니다.</p>
-      <Sample />
-
-      <button onClick={openModal}>Open Modal</button>
-      <Modal isOpen={isOpen} title="모달" onClose={closeModal}>
-        <ModalHeader title="모달" onClose={closeModal} />
-        {/* <ModalMobileHeader title="모달" onClose={closeModal} /> */}
-        <div style={{ color: '#fff' }}>바디입니다.</div>
-      </Modal>
+    <div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Profile
+          clicked
+          onClickProfile={() => {
+            return console.log('프로필사진 누름');
+          }}
+          imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493431544/rllze1.jpeg"
+        />
+        <Profile
+          size="bg"
+          selected
+          onClickDelete={() => {
+            return console.log('삭제아이콘 누름');
+          }}
+          imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493561949/theboyz1.jpeg"
+        />
+        <Profile
+          size="md"
+          clicked
+          onClickProfile={() => {
+            return console.log('프로필사진 누름');
+          }}
+          imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714493201646/nct1271.jpeg"
+        />
+        <Profile
+          size="sm"
+          selected
+          onClickDelete={() => {
+            return console.log('삭제아이콘 누름');
+          }}
+          imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714492902115/seven1.jpeg"
+        />
+      </div>
+      <div>
+        <button onClick={openModal}>Open Modal</button>
+        <Modal isOpen={isOpen} title="모달" onClose={closeModal}>
+          <ModalHeader title="모달" onClose={closeModal} />
+          {/* <ModalMobileHeader title="모달" onClose={closeModal} /> */}
+          <div style={{ color: '#fff' }}>바디입니다.</div>
+        </Modal>
+      </div>
     </div>
   );
 };
