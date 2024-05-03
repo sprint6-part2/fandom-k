3b58c1d5707cdd58c5c6581e3fa7744c49d1d92d
easyhyun00 <leejihyun0324@gmail.com>
2024-05-03 14:09:33 +0900

    🔨 Refactor: 안 쓰이는 컴포넌트 주석 처리 및 eslint 규칙에 맞게 수정

--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -1,8 +1,7 @@
-import Sample from '@/components/Sample/Sample';
 import useModal from '@/hooks/useModal';
 import Modal from '@/components/Modal';
 import ModalHeader from '@/components/Modal/components/ModalHeader';
-import ModalMobileHeader from '@/components/Modal/components/ModalMobileHeader';
+// import ModalMobileHeader from '@/components/Modal/components/ModalMobileHeader';
 import Profile from '@/components/Profile';
 
 const TestPage = () => {
@@ -44,7 +43,9 @@ const TestPage = () => {
         />
       </div>
       <div>
-        <button onClick={openModal}>Open Modal</button>
+        <button type="button" onClick={openModal}>
+          Open Modal
+        </button>
         <Modal isOpen={isOpen} title="모달" onClose={closeModal}>
           <ModalHeader title="모달" onClose={closeModal} />
           {/* <ModalMobileHeader title="모달" onClose={closeModal} /> */}
