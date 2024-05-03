84dfb69674b93bd1008430856b0c401d7536af19
Bersk3r <kiscwook@gmail.com>
2024-05-03 12:38:17 +0900

    ♻️ Refactor: PropTypes 정의 및 설명 주석 추가

--- src/components/Modal/components/ModalHeader/index.jsx
+++ src/components/Modal/components/ModalHeader/index.jsx
@@ -3,7 +3,12 @@ import ModalCloseButton from '@/assets/icons/ModalCloseButton';
 import style from '@/components/ModalHeader/styles.module.scss';
 import classNames from 'classnames';
 
-const ModalHeader = ({ onClose, title }) => {
+/**
+ * 모달 헤더 컴포넌트
+ * @param {string} title 헤더 타이틀 값
+ * @param {Event} onClose 모달 닫기 함수
+ */
+const ModalHeader = ({ title, onClose }) => {
   return (
     <div
       className={classNames(style.container, {
@@ -14,6 +19,15 @@ const ModalHeader = ({ onClose, title }) => {
       <ModalCloseButton className={style.closeButton} onClick={onClose} />
     </div>
   );
+};
+
+ModalHeader.propTypes = {
+  title: PropTypes.string,
+  onClose: PropTypes.object.isRequired,
+};
+
+ModalHeader.defaultProps = {
+  title: '모달',
 };
 
 export default ModalHeader;
--- src/components/Modal/components/ModalMobileHeader/index.jsx
+++ src/components/Modal/components/ModalMobileHeader/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import BackButton from '@/assets/icons/BackButton';
 import style from '@/components/ModalMobileHeader/styles.module.scss';
 import classNames from 'classnames';
-
+/**
+ * 모달 헤더 컴포넌트 (특정 페이지용)
+ * @param {string} title 헤더 타이틀 값
+ * @param {Event} onClose 모달 닫기 함수
+ */
 const ModalMobileHeader = ({ title, onClose }) => {
   return (
     <div className={style.container}>
@@ -10,6 +14,15 @@ const ModalMobileHeader = ({ title, onClose }) => {
       <BackButton className={style.backButton} onClick={onClose} />
     </div>
   );
+};
+
+ModalMoblieHeader.propTypes = {
+  title: PropTypes.string,
+  onClose: PropTypes.object.isRequired,
+};
+
+ModalMoblieHeader.defaultProps = {
+  title: '모달',
 };
 
 export default ModalMobileHeader;
