de825a724ef62c03c53d53da1417c5d2fcaf49ae
이지현 <leejihyun0324@gmail.com>
2024-05-03 11:04:12 +0900

    ✨ Feat: 프로필 사진 컴포넌트 구현 (#22)
    * ✨ Feat: Profile 컴포넌트 구현
    
    * ✨ Feat: 프로필 select 구현
    
    * ✨ Feat: 프로필 상단 엑스 아이콘 구현
    
    * 📚 Docs: Profile 컴포넌트 param 주석 작성
    
    * 📚 Docs: Profile 컴포넌트 주석 수정
    
    * 🚚 Rename: 프로필 사진 컴포넌트 이름 Profile 로 번경
    
    * 🔨 Refactor: 색상 상수 사용
    
    * 🔨 Refactor: 불필요한 파일 제거
    
    * 🔨 Refactor: package 파일 수정

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React from 'react';
+import classNames from 'classnames';
+import style from './styles.module.scss';
+import CheckIcon from '@/assets/icons/Check';
+import ProfileXIcon from '@/assets/icons/ProfileX';
+
+/**
+ * 프로필 사진 컴포넌트
+ * @param {string} imageUrl 이미지 주소
+ * @param {'lg' | 'bg' | 'md' | 'sm'} size 프로필 사이즈
+ * @param {boolean} clicked 프로필 클릭 유무 (핑크색)
+ * @param {boolean} selected 프로필 선택 유무 (상단 엑스)
+ * @param {()=>{}} onClickProfile 프로필 클릭 함수
+ * @param {()=>{}} onClickDelete 상단 엑스 클릭 함수
+ */
+const Profile = ({
+  imageUrl = 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg',
+  size = 'lg',
+  clicked = false,
+  selected = false,
+  onClickProfile = () => {},
+  onClickDelete = () => {},
+}) => {
+  const profileClass = classNames(style.profile, style[size]);
+  const imageClass = classNames(style.image, style[size]);
+  const clickedClass = classNames(style.clicked, style[size]);
+  const checkClass = classNames(style.check, style[size]);
+  const selectedClass = classNames(style.selected, style[size]);
+
+  const handleDeleteClick = (e) => {
+    e.stopPropagation();
+    onClickDelete();
+  };
+
+  return (
+    <div className={profileClass} onClick={onClickProfile}>
+      {selected && (
+        <ProfileXIcon className={selectedClass} onClick={handleDeleteClick} />
+      )}
+      {clicked && (
+        <>
+          <div className={clickedClass} />
+          <CheckIcon className={checkClass} />
+        </>
+      )}
+      <img className={imageClass} src={imageUrl} alt="프로필 사진" />
+    </div>
+  );
+};
+
+export default Profile;
