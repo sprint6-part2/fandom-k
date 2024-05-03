93adb8945d7c0eaa2de59dd5233a02e4f38c2931
Bersk3r <50625710+Bersk3r@users.noreply.github.com>
2024-05-03 12:47:49 +0900

    ✨ Feat: 모달 컴포넌트 구현 (#12)
    * ✨ Modal 컴포넌트 초기 생성
    
    * ♻️ Refactor: 폴더 구조 변경에 따른 파일 이름 변경
    
    * 🐛 Fix: props 내 오타 수정
    
    * ♻️ Refactor: 반환 컴포넌트 내 children Props 선언
    
    * ♻️ Refactor: 기존 Modal 및 테스트 컴포넌트 내용 작성
    
    * ✨ Feat: Modal 관련 사용자 훅 useModal 생성
    
    * 🐛 Fix: 잘못된 테스트 파일 수정
    
    * ♻️ Refactor: 바깥 클릭 시 모달이 닫히도록 구현
    
    * ♻️ Refactor: 반응형 처리를 위한 모달 헤더 부분 변경
    
    * 🐛 Fix: 특정 페이지 내에서 모달 화면이 꽉 차도록 변경
    
    * 🐛 Fix: 스타일 내 잘못된 오타 수정
    
    * ♻️ Refactor: 코드 복잡성에 따른 헤더 컴포넌트 분리
    
    * ♻️ Refactor: Modal 관련 폴더 구조 개선
    
    * 🐛 Fix: CSS 규칙에 따라 카멜케이스로 변경
    
    * ♻️ Refactor: PropTypes 정의 및 설명 주석 추가
    
    * 🐛 Fix: 각 컴포넌트 파일 내 경로 오타 수정

--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -1,6 +1,13 @@
+import Sample from '@/components/Sample/Sample';
+import useModal from '@/hooks/useModal';
+import Modal from '@/components/Modal';
+import ModalHeader from '@/components/Modal/components/ModalHeader';
+import ModalMobileHeader from '@/components/Modal/components/ModalMobileHeader';
 import Profile from '@/components/Profile';
 
 const TestPage = () => {
+  const [isOpen, openModal, closeModal] = useModal();
+
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <Profile
@@ -34,6 +41,15 @@ const TestPage = () => {
         }}
         imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Fandom-K/idol/1714492902115/seven1.jpeg"
       />
+      <p>공통 컴포넌트 만든거는 test 페이지에서 테스트합니다.</p>
+      <Sample />
+
+      <button onClick={openModal}>Open Modal</button>
+      <Modal isOpen={isOpen} title="모달" onClose={closeModal}>
+        <ModalHeader title="모달" onClose={closeModal} />
+        {/* <ModalMobileHeader title="모달" onClose={closeModal} /> */}
+        <div style={{ color: '#fff' }}>바디입니다.</div>
+      </Modal>
     </div>
   );
 };
