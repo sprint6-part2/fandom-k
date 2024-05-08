d03a7093e26a866732a3daaec7d762886fc6df9c
easyhyun00 <leejihyun0324@gmail.com>
2024-05-08 22:55:08 +0900

    🔨 Refactor: 종민님 데이터 가져오는 Hook 사용하여 구현

---
+++ src/hooks/useLoad.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+/**
+ *
+ * @param {func} getData api 요청 함수
+ *
+ */
+export const useLoad = (getData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
+
+  const handleLoad = async (...arg) => {
+    let result;
+    try {
+      setLoadingError(null);
+      setIsLoading(true);
+      result = await getData(...arg);
+      return result;
+    } catch (error) {
+      setLoadingError(error);
+      return null;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { isLoading, loadingError, handleLoad };
+};
--- src/pages/ListPage/Donation/index.jsx
+++ src/pages/ListPage/Donation/index.jsx
@@ -1,42 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import style from './styles.module.scss';
 import Carousel from '@/components/Carousel';
 import { carouselSettings } from './carouselSetting';
 import Card from './components/Card';
+import Logo from '@/assets/icons/Logo';
+import { useLoad } from '@/hooks/useLoad';
 import { getDonations } from '@/apis/getDonations';
 
 const Donation = () => {
+  const { isLoading, loadingError, handleLoad } = useLoad(getDonations);
   const [donationList, setDonationList] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+
+  const handleDonationLoad = async () => {
+    const donations = await handleLoad();
+    if (donations) {
+      setDonationList(donations.list);
+    }
+  };
 
   useEffect(() => {
-    getDonations()
-      .then((data) => {
-        setDonationList(data);
-        setError(null);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setDonationList(null);
-        setError(error.message);
-        setIsLoading(false);
-      });
+    handleDonationLoad();
   }, []);
 
   return (
     <section className={style.container}>
       <h2 className={style.title}>후원을 기다리는 조공</h2>
       {isLoading && <p className={style.loading}>데이터 가져오는 중...</p>}
-      {error && (
+      {loadingError && (
         <div className={style.error}>
-          <h3>⚠️ 문제 발생 ⚠️</h3>
-          <p>{error}</p>
+          <Logo />
+          <h3>문제가 발생했습니다</h3>
+          <p>{loadingError}</p>
         </div>
       )}
       {donationList && (
         <Carousel customSettings={carouselSettings}>
-          {donationList.list.map((item) => {
+          {donationList.map((item) => {
             return <Card item={item} key={item.id} />;
           })}
         </Carousel>
