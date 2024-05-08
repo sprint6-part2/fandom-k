e898ada2e95218853ae57bd33800e55db304bae4
easyhyun00 <leejihyun0324@gmail.com>
2024-05-08 15:12:32 +0900

    ✨ Feat: 후원목록  데이터 불러오기

--- src/apis/getDonations.js
+++ src/apis/getDonations.js
@@ -2,7 +2,6 @@ import { baseAxios } from './api';
 import { BACKEND_ENDPOINT } from '@/constants/endpoint';
 
 export const getDonations = async (pageSize = 10) => {
-  console.log(BACKEND_ENDPOINT);
   const response = await baseAxios.get(`/donations?pageSize=${pageSize}`);
 
   return response;
--- src/pages/ListPage/Donation/index.jsx
+++ src/pages/ListPage/Donation/index.jsx
@@ -7,30 +7,29 @@ import testData from './mock.json';
 import { getDonations } from '@/apis/getDonations';
 
 const Donation = () => {
-  // const [donationList, setDonationList] = useState([]);
+  const [donationList, setDonationList] = useState(null);
 
-  console.log(import.meta.env);
-  console.log(import.meta.env.VITE_BACKEND_ENDPOINT);
-
-  // useEffect(() => {
-  //   getDonations()
-  //     .then((data) => {
-  //       console.log(data);
-  //       setDonationList(data); // 데이터를 상태에 설정해줍니다.
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error fetching donations:', error);
-  //     });
-  // }, []);
+  useEffect(() => {
+    getDonations()
+      .then(({ data }) => {
+        console.log(data);
+        setDonationList(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching donations:', error);
+      });
+  }, []);
 
   return (
     <section className={style.container}>
       <h2 className={style.title}>후원을 기다리는 조공</h2>
-      <Carousel customSettings={carouselSettings}>
-        {testData.list.map((item) => {
-          return <Card item={item} key={item.id} />;
-        })}
-      </Carousel>
+      {donationList && (
+        <Carousel customSettings={carouselSettings}>
+          {donationList.list.map((item) => {
+            return <Card item={item} key={item.id} />;
+          })}
+        </Carousel>
+      )}
     </section>
   );
 };
