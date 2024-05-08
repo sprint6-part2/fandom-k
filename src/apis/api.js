06a8fb35a9696d07e95c9bc173b6ec144ceb6d50
easyhyun00 <leejihyun0324@gmail.com>
2024-05-08 15:57:39 +0900

    ✨ Feat: 데이터 로딩, 에러 처리 구현

--- src/apis/api.js
+++ src/apis/api.js
@@ -3,4 +3,5 @@ import { BACKEND_ENDPOINT } from '@/constants/endpoint';
 
 export const baseAxios = axios.create({
   baseURL: BACKEND_ENDPOINT,
+  timeout: 20000,
 });
--- src/pages/ListPage/Donation/index.jsx
+++ src/pages/ListPage/Donation/index.jsx
@@ -3,26 +3,37 @@ import style from './styles.module.scss';
 import Carousel from '@/components/Carousel';
 import { carouselSettings } from './carouselSetting';
 import Card from './components/Card';
-import testData from './mock.json';
 import { getDonations } from '@/apis/getDonations';
 
 const Donation = () => {
   const [donationList, setDonationList] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     getDonations()
-      .then(({ data }) => {
-        console.log(data);
+      .then((data) => {
         setDonationList(data);
+        setError(null);
+        setIsLoading(false);
       })
-      .catch((error) => {
-        console.error('Error fetching donations:', error);
+      .catch((err) => {
+        setDonationList(null);
+        setError(err.message);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <section className={style.container}>
       <h2 className={style.title}>후원을 기다리는 조공</h2>
+      {isLoading && <p className={style.loading}>데이터 가져오는 중...</p>}
+      {error && (
+        <div className={style.error}>
+          <h3>⚠️ 문제 발생 ⚠️</h3>
+          <p>{error}</p>
+        </div>
+      )}
       {donationList && (
         <Carousel customSettings={carouselSettings}>
           {donationList.list.map((item) => {
