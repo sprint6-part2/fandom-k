1679dc99e4e4e9345151a1e97bcdaabdf51fcb7a
easyhyun00 <leejihyun0324@gmail.com>
2024-05-11 17:57:41 +0900

    🔨 Refactor: 라우터에서 Provider 감싸기

--- src/pages/ListPage/index.jsx
+++ src/pages/ListPage/index.jsx
@@ -3,20 +3,17 @@ import Header from '@/components/Header';
 import style from './styles.module.scss';
 import Credit from './Credit';
 import MonthlyChart from './MonthlyChart';
-import { CreditProvider } from '@/contexts/CreditContext';
 
 const ListPage = () => {
   return (
-    <CreditProvider>
-      <div className={style.container}>
-        <Header />
-        <main className={style.main}>
-          <Credit />
-          <Donation />
-          <MonthlyChart />
-        </main>
-      </div>
-    </CreditProvider>
+    <div className={style.container}>
+      <Header />
+      <main className={style.main}>
+        <Credit />
+        <Donation />
+        <MonthlyChart />
+      </main>
+    </div>
   );
 };
 
