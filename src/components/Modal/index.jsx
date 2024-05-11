b5e72d993ab0b70fdd054d8fd24c18ac4ce6bfc9
easyhyun00 <leejihyun0324@gmail.com>
2024-05-11 14:07:36 +0900

    🔨 Refactor: 모달 Portal 을 사용하여 구현

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import style from '@/components/Modal/styles.module.scss';
 import { motion, AnimatePresence } from 'framer-motion';
+import { createPortal } from 'react-dom';
 
 /**
  * 프로필 사진 컴포넌트
@@ -9,19 +10,15 @@ import { motion, AnimatePresence } from 'framer-motion';
  * @param {Event} onClose 모달 닫기 함수
  * @param {object} children 부모 컴포넌트로부터 받은 내용
  */
-
 const Modal = ({ isOpen, onClose, children }) => {
   const Ref = useRef(null);
 
-  const handleBackDrop = (e) => {
-    return e.target === Ref.current && onClose();
-  };
-
-  return (
+  return createPortal(
     <AnimatePresence>
       {isOpen && (
-        <motion.div
+        <motion.aside
           className={style.modalBackDrop}
+          ref={Ref}
           onClick={onClose}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -29,22 +26,21 @@ const Modal = ({ isOpen, onClose, children }) => {
         >
           <div
             className={style.modalView}
-            onClick={(e) => {
-              return e.stopPropagation();
-            }}
+            onClick={(e) => e.target === Ref.current && onClose()}
           >
             {children}
           </div>
-        </motion.div>
+        </motion.aside>
       )}
-    </AnimatePresence>
+    </AnimatePresence>,
+    document.body,
   );
 };
 
 Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
-  onClose: PropTypes.object.isRequired,
-  children: PropTypes.object.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default Modal;
