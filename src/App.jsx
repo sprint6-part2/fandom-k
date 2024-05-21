24f6842aefe86b05b9f82ec7d4de41435c969eab
easyhyun00 <leejihyun0324@gmail.com>
2024-05-21 10:26:23 +0900

    🔨 Refactor: toastContainer app.jsx 로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Header from '@/components/Header';
 import { Background } from '@/components/Background';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
   const location = useLocation();
   const isListOrMyPage =
@@ -14,6 +17,14 @@ const App = () => {
     <Background>
       {isListOrMyPage && <Header />}
       <Outlet />
+      <ToastContainer
+        autoClose={1500}
+        style={{
+          margin: '50px 30px 0 0',
+          padding: '0 16px',
+        }}
+        position="top-right"
+      />
     </Background>
   );
 };
